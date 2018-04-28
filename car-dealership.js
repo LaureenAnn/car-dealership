718cb77460efc254a19742088c6710ed04e8aea0
Laureen <laureenann@gmail.com>
2018-04-28 17:48:43 +0000

    Car dealership with rental car avaibility by car type

--- car-dealership.js
+++ car-dealership.js
@@ -4,7 +4,7 @@ var carDealership = {
         rentalPrice: "$80",
         availability: 5,
         requestResponse: function() {
-            console.log(`Sedan availability is ${this.availability}.`);
+            console.log(`Sedan availability is ${this.availability}.  The rental price is ${this.rentalPrice}.`);
         }
     },
     
@@ -12,7 +12,7 @@ var carDealership = {
         rentalPrice: "$100",
         availability: 4,
         requestResponse: function() {
-            console.log(`SUV availability is ${this.availability}.`);
+            console.log(`SUV availability is ${this.availability}. The rental price is ${this.rentalPrice}.`);
         }
     },
     
@@ -20,7 +20,7 @@ var carDealership = {
         rentalPrice: "$120",
         availability: 0,
         requestResponse: function() {
-            console.log(`Sports availability is ${this.availability}.`);
+            console.log(`Sports availability is ${this.availability}.  The rental price is ${this.rentalPrice}.`);
         }
     }
 
